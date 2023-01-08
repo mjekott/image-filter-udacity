0a228a82cc07e18364347e121256e7d3abbdc9b8
mjekott <ekottmfon@yahoo.com>
2023-01-08 07:21:05 +0100

    refactor: filterimage endpoint

--- src/server.ts
+++ src/server.ts
@@ -12,29 +12,40 @@ import { deleteLocalFiles, filterImageFromURL } from "./util/util";
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  /**
-   * GET /filteredimage?image_url={{URL}}
-   * returns filtered image file
-   */
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+  //! END @TODO1
   app.get("/filteredimage", async (req: Request, res: Response) => {
-    const { image_url } = req.query;
-    if (!image_url) {
-      return res.status(400).json({
-        message: "image_url query is required",
-      });
+    const imageUrl: string = req.query.image_url;
+    if (!imageUrl) {
+      res.status(400).send("image url not specify. Please specify image url");
     }
     try {
-      const filteredImage = await filterImageFromURL(image_url);
-      return res.status(200).sendFile(filteredImage, async () => {
-        await deleteLocalFiles([filteredImage]);
+      const filteredImage: string = await filterImageFromURL(imageUrl);
+      res.status(200).sendFile(filteredImage);
+      console.log("imagepath:", filteredImage);
+      // deleting the image after response is sent
+      res.on("finish", function () {
+        deleteLocalFiles([filteredImage]);
       });
-    } catch (error: Error) {
-      return res.status(400).json({
-        message: error!.message,
-      });
+    } catch (error) {
+      res.status(422).send(error);
     }
   });
-
   // Root Endpoint
   // Displays a simple message to the user
   app.get("/", async (req, res) => {
